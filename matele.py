871eb84b2b2102def0ff8c216fa4d7ee9c71b604
naphatcmkl <85152768+naphatcmkl@users.noreply.github.com>
2023-02-07 18:15:47 +0700

    still fixing nanae mess

--- matele.py
+++ matele.py
@@ -1,6 +1,45 @@
 import numpy as np
 
 
+def multback(a):
+    i = len(a)-1
+    while i > 0:
+        i -= 1
+    return
+
+
+def mult(a, b):
+    return a*b
+
+
+def checkzero(e):
+    iszero = True
+    for i in range(len(e)):
+        print(i)
+        for j in range(i+1):
+            if e[i, j] != 0:
+                iszero = False
+                break
+
+    return iszero
+
+
+def notzero(e):
+    index = []
+    for i in range(len(e)):
+        print(i)
+        for j in range(i+1):
+            if e[i, j] != 0:
+                index = [i, j]
+                break
+
+    return index
+
+
+def swapper(e, i, j):
+    return e
+
+
 def square(e):
     total = e.shape
     srow = total[0]
@@ -8,6 +47,9 @@ def square(e):
     h1 = np.eye(srow)
     print("print identity")
     print(h1)
+    answer = {}
+    answer["original"] = e
+    answer["eye"] = []
     if e[0, 0] == 0:
         i = 0
         swap = False
@@ -15,12 +57,14 @@ def square(e):
             i += 1
         if e[i, 0] != 0:
             swap = True
-        print(swap)
         if swap:
             print(i)
             e[(0, i)] = e[(i, 0)]
             print(e)
             print("end swap")
+            answer["swapped"] = e
+    while not checkzero(e):
+        x = notzero(e)
 
     return e
 
