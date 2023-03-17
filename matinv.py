f52281e90fb5bad5d24eb6664494fcb8015a5fe6
naphatcmkl <85152768+naphatcmkl@users.noreply.github.com>
2023-03-17 14:28:27 +0700

    start inv

--- matinv.py
+++ matinv.py
@@ -4,9 +4,23 @@ import numpy as np
 # det != 0
 
 
-def make_det():
+def make_det(m, a):
     print("deting...")
+    det = 1
+    a["det"] = det
+    return
 
 
 def make_rref():
     print("rrefing...")
+
+
+def init_invert(e):
+    print("checking mat if it's invertible")
+    print('copy original mat')
+    ans = {}
+    ans["det"] = 0
+    fakeE = np.array(e)
+    ans = make_det(fakeE, ans)
+    if ans["det"] != 0:
+        return ans
